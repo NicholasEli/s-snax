3fd41354a6705885d8ae9add879baa5a3bc08236
NicholasEli <nicholasbydesign@gmail.com>
2025-12-14 17:14:38 -0800

    add init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+	const toggle_sort = document.getElementById('toggle-sort');
+	const sortable_list = document.querySelector('s-sortable-list');
+
+	toggle_sort.addEventListener('change', (event) =>
+		sortable_list.setAttribute('sorting', event.target.checked)
+	);
+
+	document
+		.getElementById('sortable-list')
+		.addEventListener('change', (event) => console.log(event.detail.items));
+};
---
+++ s-sortable-list.js
@@ -0,0 +1,168 @@
+const sheet = new CSSStyleSheet();
+sheet.replaceSync(`
+  ul { position: relative; margin: 0; padding: 0; list-style: none; }
+  ul:has(.active) li { cursor: grabbing;}
+  li { touch-action: none; user-select: none; -webkit-user-drag: none; cursor: grab; }
+  li.active { opacity: 0; }
+
+  #s-sortable-list-clone { 
+  	position: fixed; 
+  	top: 0; 
+  	left: 0; 
+  	z-index: 2; 
+  	list-style: none;
+		pointer-events: none;
+		transform: translate3d(0, 0, 0);
+		will-change: transform;
+		background-color: #fff;
+		opacity: 0.75;
+  }
+
+  #s-sortable-list-clone.active { opacity: 0 }
+`);
+
+class SortableListItem extends HTMLElement {
+	connectedCallback() {
+		if (!this.closest('s-sortable-list')) {
+			console.warn('<s-sortable-list-item> must be used inside <s-sortable-list>');
+		}
+
+		if (!this.id) {
+			console.warn('<s-sortable-list-item> must be have ID to render');
+		}
+	}
+}
+
+class SortableList extends HTMLElement {
+	constructor() {
+		super();
+		this.shadow = this.attachShadow({ mode: 'open' });
+		this.shadow.adoptedStyleSheets = [sheet];
+		this.headers = [];
+		this.items = [];
+		this.dragging = null;
+	}
+
+	connectedCallback() {
+		this.items = Array.from(this.querySelectorAll('s-sortable-list-item')).filter(
+			(item) => item.id
+		);
+		this.render();
+	}
+
+	static get observedAttributes() {
+		return ['sorting'];
+	}
+
+	attributeChangedCallback(attribute, prev, next) {
+		if (prev === next) return;
+
+		this[attribute] = next;
+
+		this.render();
+	}
+
+	type() {
+		let type = this.getAttribute('type');
+		if (!type) return 'unordered';
+
+		type = type.toLowerCase();
+		if (type !== 'unordered' && type !== 'ordered') return 'unordered';
+
+		return type;
+	}
+
+	clonePOS(event) {
+		const clone = this.shadowRoot.getElementById('s-sortable-list-clone');
+
+		if (!event || !clone) return null;
+
+		const bounds = clone.getBoundingClientRect();
+		const yDiff = bounds.height / 2;
+		const xDiff = bounds.width / 2;
+		clone.style.top = event.clientY - yDiff + 'px';
+		clone.style.left = event.clientX - xDiff + 'px';
+	}
+
+	onPointerDown(event, index) {
+		const elements = this.shadowRoot.querySelectorAll('li');
+		const element = elements[index];
+
+		element.classList.add('active');
+
+		this.dragging = {
+			index,
+			item: this.items[index]
+		};
+
+		const clone = element.cloneNode(true);
+		clone.id = 's-sortable-list-clone';
+		this.shadowRoot.append(clone);
+
+		setTimeout(() => {
+			this.clonePOS(event);
+			clone.classList.remove('active');
+		}, 50);
+	}
+
+	onPointerMove(event) {
+		const clone = this.shadowRoot.getElementById('s-sortable-list-clone');
+
+		if (!this.dragging || !clone) return null;
+
+		this.clonePOS(event);
+	}
+
+	onPointerUp(index) {
+		if (!this.dragging) return null;
+
+		const clone = this.shadowRoot.getElementById('s-sortable-list-clone');
+		clone.remove();
+
+		this.items.splice(this.dragging.index, 1);
+		this.items.splice(index, 0, this.dragging.item);
+
+		this.dragging = null;
+		this.render();
+
+		this.dispatchEvent(
+			new CustomEvent('change', {
+				detail: {
+					items: this.items.map((item) => item.id)
+				},
+				bubbles: true,
+				composed: true
+			})
+		);
+	}
+
+	render() {
+		const sorting = this.getAttribute('sorting') == 'true';
+
+		this.shadow.innerHTML = `
+			<ul>
+				${this.items
+					.filter((item) => item.id)
+					.map((item) => {
+						return `
+					<li>
+						<s-grid gridTemplateColumns="${sorting ? 'auto 1fr' : '1fr'}" justifyContent="start">
+							${sorting ? `<s-grid-item><s-icon type="drag-handle"></s-icon></s-grid-item>` : ''}
+							<s-grid-item>${item.textContent}</s-grid-item>
+						</s-grid>
+					</li>
+				`;
+					})
+					.join('')}<ul>
+		`;
+
+		this.shadowRoot.querySelectorAll('li').forEach((li, index) => {
+			li.addEventListener('pointerdown', (event) => this.onPointerDown(event, index));
+			li.addEventListener('pointermove', (event) => this.onPointerMove(event));
+			li.addEventListener('pointerup', (event) => this.onPointerUp(index));
+		});
+	}
+}
+
+customElements.define('s-sortable-list', SortableList);
+customElements.define('s-sortable-list-item', SortableListItem);
